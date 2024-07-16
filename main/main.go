a5acee70f615f95c851be33a727c9a86ab104ba6
Nick2022 <876927113@qq.com>
2024-07-16 16:00:41 +0800

    geth practise.

--- main/main.go
+++ main/main.go
@@ -1,25 +1,64 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"log"
+	"math"
+	"math/big"
 	"time"
 )
 
 func main() {
 	t := time.Now()
-	//client, err := ethclient.Dial("https://cloudflare-eth.com") 	// 远端
+	client, err := ethclient.Dial("https://cloudflare-eth.com") // 远端
 
 	// npm install -g ganache-cli 	安装
 	// ganache-cli 	运行
 	// http://localhost:8584
-	client, err := ethclient.Dial("http://localhost:8584") // 本地
+	//client, err := ethclient.Dial("http://localhost:8584") // 本地
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 		return
 	}
 	cost := time.Since(t)
-	log.Printf("cost: %v\n", cost)
-	log.Println("Connected to Ethereum client")
-	_ = client
+	fmt.Printf("cost: %v\n", cost)
+	fmt.Println("Connected to Ethereum client")
+	//_ = client
+
+	address := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	fmt.Println("address: ", address)
+	fmt.Println("address: ", address.Hex())
+	fmt.Println("address: ", address.Bytes())
+
+	// nil 表示最新区块
+	balance, err := client.BalanceAt(context.Background(), address, big.NewInt(5532992)) // nil
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("balance: ", balance)
+
+	blockNum := big.NewInt(5532993)
+	balance, err = client.BalanceAt(context.Background(), address, blockNum)
+	fmt.Println("blockNum: ", blockNum, ", balance: ", balance)
+
+	if balance == nil {
+		fmt.Println("balance is nil")
+		return
+	}
+
+	fBalance := new(big.Float)
+	fBalance.SetString(balance.String())
+	ethValue := new(big.Float).Quo(fBalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println("ethValue: ", ethValue)
+
+	// 有时您想知道待处理的账户余额是多少，例如，在提交或等待交易确认后
+	pendingBalanceAt, err := client.PendingBalanceAt(context.Background(), address)
+	if err != nil {
+		return
+	}
+	fmt.Println("pendingBalanceAt: ", pendingBalanceAt)
 }
